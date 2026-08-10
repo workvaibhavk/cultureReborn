9fa55b901d94e38cf60c71c31201e4eb2f3be98a
Vaibhav Kamble <itsvaibhav.work@gmail.com>
2026-08-10 18:56:50 +0530

    Remove BrowserRouter from layout component
    Removed BrowserRouter wrapper from layout.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { BrowserRouter } from "react-router-dom";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black">
-        <BrowserRouter>{children}</BrowserRouter>
+        {children}
       </body>
     </html>
   );
